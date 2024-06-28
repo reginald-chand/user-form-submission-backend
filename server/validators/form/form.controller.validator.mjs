21b69cb490989445b93bde5ce960465d1eb08c34
Reginald Chand <reginald-chand@outlook.com>
2024-06-29 01:48:18 +1200

    Create form.controller.validator.mjs

---
+++ server/validators/form/form.controller.validator.mjs
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const formControllerValidator = Joi.object({
+  _csrf: Joi.string().required(),
+
+  firstName: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
+  lastName: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+
+  message: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
+});
