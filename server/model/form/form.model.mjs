58ef80f8170414fb0406f7d0429430d2c5960a23
Reginald Chand <reginald-chand@outlook.com>
2024-06-29 01:47:48 +1200

    Create form.model.mjs

---
+++ server/model/form/form.model.mjs
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const formSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    message: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const FormModel = mongoose.model("Form", formSchema);
