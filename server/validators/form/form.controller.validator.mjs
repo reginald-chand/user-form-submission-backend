3c0a1afbcbd9347a72ec8dd65cb873c1f1619262
Reginald Chand <reginald-chand@outlook.com>
2024-06-30 12:12:56 +1200

    Remove csrf requirement

--- server/validators/form/form.controller.validator.mjs
+++ server/validators/form/form.controller.validator.mjs
@@ -1,8 +1,6 @@
 import Joi from "joi";
 
 export const formControllerValidator = Joi.object({
-  _csrf: Joi.string(),
-
   firstName: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
   lastName: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
 
