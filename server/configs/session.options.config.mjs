469ae037eab483a41d3e863e49ea924b6cd642fb
Reginald Chand <reginald-chand@outlook.com>
2024-06-30 12:11:54 +1200

    Add express session secret fallback

--- server/configs/session.options.config.mjs
+++ server/configs/session.options.config.mjs
@@ -1,5 +1,5 @@
 export const sessionOptions = {
-  secret: process.env.EXPRESS_SESSION_SECRET,
+  secret: process.env.EXPRESS_SESSION_SECRET || "EXPRESS_SESSION_SECRET",
   resave: false,
   saveUninitialized: false,
   cookie: { secure: true },
